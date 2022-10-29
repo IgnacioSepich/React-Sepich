fae1d943818645c67d15435f1112549764271cf8
ignacio sepich <ignaciosepich@gmail.com>
2022-10-29 09:22:52 -0300

    Proyecto Final

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -2,6 +2,6 @@ export const customFetch = (products) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve (products)
-        }, 3000)
+        }, 2000)
     })
 }
